61ec4fa13a2cb3aeac87d826a78648e7b51860ce
Maciek <maciej.kaminski@orange.com>
2016-07-26 14:25:17 +0200

    [Added oracle docs]

--- src/main/java/cdi/custom/scope/cdr/test/CdrTest.java
+++ src/main/java/cdi/custom/scope/cdr/test/CdrTest.java
@@ -32,31 +32,35 @@ import pl.orange.isep.model.event.cdr.CDRVoucherBalanceRechargeEvent;
 @author mkamin
 */
 
-// enable logging programatically
+/**
+FROM DOCS: http://www.oracle.com/technetwork/topics/11-2.pdf
 
-//		javax.management.MBeanServer mbs = java.lang.management.ManagementFactory.getPlatformMBeanServer();
-//		String loader = Thread.currentThread().getContextClassLoader().toString().replaceAll("[,=:\"]+", "");
-//		javax.management.ObjectName name = new javax.management.ObjectName("com.oracle.jdbc:type=diagnosability,name=" + loader);
-//		mbs.setAttribute(name, new javax.management.Attribute("LoggingEnabled", true));
+enable logging programatically
 
-//		System.out.println("LoggingEnabled = " + mbs.getAttribute(name, "LoggingEnabled"));
+javax.management.MBeanServer mbs = java.lang.management.ManagementFactory.getPlatformMBeanServer();
+String loader = Thread.currentThread().getContextClassLoader().toString().replaceAll("[,=:\"]+", "");
+javax.management.ObjectName name = new javax.management.ObjectName("com.oracle.jdbc:type=diagnosability,name=" + loader);
+mbs.setAttribute(name, new javax.management.Attribute("LoggingEnabled", true));
 
-//OJDBC logging
+System.out.println("LoggingEnabled = " + mbs.getAttribute(name, "LoggingEnabled"));
+		
+OJDBC logging
 
-//uruchomienie z wiersza polecen:
-//java -Djava.util.logging.config.file=/home/mkamin/Desktop/myConfig.properties -Doracle.jdbc.Trace=true -cp .:/home/mkamin/.m2/repository/pl/orange/isep/isep-model/0.1-SNAPSHOT/isep-model-0.1-SNAPSHOT.jar:/home/mkamin/git/isep2/isep-forked/isep/isep-api/target/isep-api-0.1-SNAPSHOT.jar:/home/mkamin/.m2/repository/oracle/ojdbc/ojdbc_g/1.0/ojdbc_g-1.0.jar cdi.custom.scope.cdr.test.CdrTest
+uruchomienie z wiersza polecen:
+java -Djava.util.logging.config.file=/home/mkamin/Desktop/myConfig.properties -Doracle.jdbc.Trace=true -cp .:/home/mkamin/.m2/repository/pl/orange/isep/isep-model/0.1-SNAPSHOT/isep-model-0.1-SNAPSHOT.jar:/home/mkamin/git/isep2/isep-forked/isep/isep-api/target/isep-api-0.1-SNAPSHOT.jar:/home/mkamin/.m2/repository/oracle/ojdbc/ojdbc_g/1.0/ojdbc_g-1.0.jar cdi.custom.scope.cdr.test.CdrTest
 
-//====================
-//vim myConfig.properties
-//.level=SEVERE
-//oracle.jdbc.level=SEVERE
-//oracle.jdbc.aq.level=ALL
-//oracle.jdbc.handlers=java.util.logging.FileHandler
-//java.util.logging.FileHandler.level=FINE
-//java.util.logging.FileHandler.pattern=jdbc1.log
-//java.util.logging.FileHandler.count=1
-//java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter
-//====================	
+====================
+vim myConfig.properties
+.level=SEVERE
+oracle.jdbc.level=SEVERE
+oracle.jdbc.aq.level=ALL
+oracle.jdbc.handlers=java.util.logging.FileHandler
+java.util.logging.FileHandler.level=FINE
+java.util.logging.FileHandler.pattern=jdbc1.log
+java.util.logging.FileHandler.count=1
+java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter
+====================
+*/
 
 
 public class CdrTest {
